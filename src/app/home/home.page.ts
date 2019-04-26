48707305a225aeec9d8858bad02aab433b4c200d
Aluno <you@example.com>
2019-04-25 21:23:09 -0300

    Criação de uma lista de tarefas.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  title = "Lista | Tarefas";
+  tarefas = [
+    {
+      "descricao": "Ir para o IFPI",
+      "horario": "18:00"
+    },
+
+    {
+      "descricao": "Dormir",
+      "horario": "05:00"
+    },
+
+    {
+      "descricao": "Comer",
+      "horario": "12:00"
+    },
+
+  ];
+
+
+  nova_tarefa = this.criar_nova_tarefa();
+
+
+  add() {
+    this.tarefas.push(this.nova_tarefa);
+    this.nova_tarefa = this.criar_nova_tarefa();
+  }
+
+  criar_nova_tarefa() {
+    return {
+      "descricao": "",
+      "horario": ""
+
+    }
+  }
 
 }
